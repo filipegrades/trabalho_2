bc113287b36dc42482173bb5eca019dbe30a593f
filipegrades <filipe.grades@estudante.ufjf.br>
2026-06-15 15:36:07 -0300

    .

--- trabalho_2_hil/main.c
+++ trabalho_2_hil/main.c
@@ -5,6 +5,19 @@
 #include "device.h"
 #include "board.h"
 #include "scicomm.h"
+#include <math.h>
+
+// Defines
+#define pi 3.141592f 
+#define vdc 400.0f
+#define L 3e-3f
+#define R 0.037699f
+#define dT 5e-6f
+#define fg 60.0f
+#define km1 ((2*L-dT*R)/(2*L+dT*R))
+#define km2 ((dT*vdc)/(4*L+2*dT*R))
+#define km3 (dT/(4*L+2**dT*R))
+
 
 #pragma DATA_SECTION(e,"Cla1ToCpuMsgRAM");
 float e[3]={0};
@@ -23,6 +36,18 @@ float32_t theta = 0.0f;
 #pragma DATA_SECTION(sref,"Cla1ToCpuMsgRAM");
 float sref;
 
+// Variaveis do modelo
+float il[2] = {0};
+float S[2] = {0};
+float vt[2] = {0};
+float Si[4] = {0};
+float vt_amplitude = sqrt(2)*220;
+float vt_dTheta = 2*pi*fg*dT;
+float vt_theta = 0.0;
+float vt_phi = 0.0;
+bool habilitaCalculo = false;
+
+
 //
 // Funcao Principal
 //
@@ -40,7 +65,18 @@ void main(void)
 
     while (1)
     {
-
+        if(habilitaCalculo)
+        {
+            habilitaCalculo = false;
+            vt[0] = vt_amplitude*sin(vt_theta+vt_phi);
+            vt_theta = ((vt_theta+vt_dTheta)<=2*pi) ? (vt_theta+vt_dTheta) : (2*pi - vt_theta+vt_dTheta);
+            S[0] = 0.5*((Si[0]+Si[3])-(Si[1]+Si[2]));
+            il[0] = km1*il[1] + km2*(S[1]+S[0]) - km3*(vt[1]+vt[0]);
+            S[1] = S[0];
+            il[1] = il[0];
+            vt[1] = vt[0];
+            
+        }
     }
 }
 
